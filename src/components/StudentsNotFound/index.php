9f225a550f442da0dc1122c7aa2d7b096764cdc8
henriqueclaranhan <henrique.claranhan@outlook.com>
2021-10-01 15:16:36 -0400

    create students not found component

---
+++ src/components/StudentsNotFound/index.php
@@ -0,0 +1,11 @@
+<style>
+    <?php
+        include_once "src/components/StudentsNotFound/style.css";
+    ?>
+</style>
+
+<div id="students-not-found-box">
+    <img src="public/assets/icons/people-circle.svg" alt="">
+    <h2>Nenhum aluno encontrado!</h2>
+    <p>Clique no botão "Cadastrar aluno" para adicionar um novo aluno ao banco de dados.</p>
+</div>
